5de24e5045aac510aaa40065bdac947def0acc8e
YukiMinami22 <phoenix22@tutanota.com>
2017-07-19 01:07:21 -0700

    HighMaps

---
+++ data.js
@@ -0,0 +1,258 @@
+data = [
+                {
+                                "hc-key": "Alabama",
+                                "value1": "26",
+                                "value2": "17"
+                },
+                {
+                                "hc-key": "Alaska",
+                                "value1": "35",
+                                "value2": "32"
+                },
+                {
+                                "hc-key": "Arizona",
+                                "value1": "38",
+                                "value2": "35"
+                },
+                {
+                                "hc-key": "Arkansas",
+                                "value1": "32",
+                                "value2": "25"
+                },
+                {
+                                "hc-key": "California",
+                                "value1": "29",
+                                "value2": "27"
+                },
+                {
+                                "hc-key": "Colorado",
+                                "value1": "43",
+                                "value2": "37"
+                },
+                {
+                                "hc-key": "Connecticut",
+                                "value1": "41",
+                                "value2": "36"
+                },
+                {
+                                "hc-key": "Delaware",
+                                "value1": "37",
+                                "value2": "30"
+                },
+                {
+                                "hc-key": "Florida",
+                                "value1": "42",
+                                "value2": "26"
+                },
+                {
+                                "hc-key": "Georgia",
+                                "value1": "35",
+                                "value2": "28"
+                },
+                {
+                                "hc-key": "Hawaii",
+                                "value1": "38",
+                                "value2": "30"
+                },
+                {
+                                "hc-key": "Idaho",
+                                "value1": "38",
+                                "value2": "34"
+                },
+                {
+                                "hc-key": "Illinois",
+                                "value1": "37",
+                                "value2": "32"
+                },
+                {
+                                "hc-key": "Indiana",
+                                "value1": "50",
+                                "value2": "39"
+                },
+                {
+                                "hc-key": "Iowa",
+                                "value1": "44",
+                                "value2": "37"
+                },
+                {
+                                "hc-key": "Kansas",
+                                "value1": "41",
+                                "value2": "33"
+                },
+                {
+                                "hc-key": "Kentucky",
+                                "value1": "40",
+                                "value2": "28"
+                },
+                {
+                                "hc-key": "Louisiana",
+                                "value1": "30",
+                                "value2": "18"
+                },
+                {
+                                "hc-key": "Maine",
+                                "value1": "41",
+                                "value2": "35"
+                },
+                {
+                                "hc-key": "Maryland",
+                                "value1": "40",
+                                "value2": "35"
+                },
+                {
+                                "hc-key": "Massachusetts",
+                                "value1": "54",
+                                "value2": "51"
+                },
+                {
+                                "hc-key": "Michigan",
+                                "value1": "34",
+                                "value2": "29"
+                },
+                {
+                                "hc-key": "Minnesota",
+                                "value1": "53",
+                                "value2": "48"
+                },
+                {
+                                "hc-key": "Mississippi",
+                                "value1": "30",
+                                "value2": "22"
+                },
+                {
+                                "hc-key": "Missouri",
+                                "value1": "38",
+                                "value2": "31"
+                },
+                {
+                                "hc-key": "Montana",
+                                "value1": "41",
+                                "value2": "39"
+                },
+                {
+                                "hc-key": "Nebraska",
+                                "value1": "46",
+                                "value2": "38"
+                },
+                {
+                                "hc-key": "Nevada",
+                                "value1": "32",
+                                "value2": "26"
+                },
+                {
+                                "hc-key": "New Hampshire",
+                                "value1": "51",
+                                "value2": "46"
+                },
+                {
+                                "hc-key": "New Jersey",
+                                "value1": "47",
+                                "value2": "46"
+                },
+                {
+                                "hc-key": "New Mexico",
+                                "value1": "27",
+                                "value2": "21"
+                },
+                {
+                                "hc-key": "New York",
+                                "value1": "35",
+                                "value2": "31"
+                },
+                {
+                                "hc-key": "North Carolina",
+                                "value1": "44",
+                                "value2": "33"
+                },
+                {
+                                "hc-key": "North Dakota",
+                                "value1": "45",
+                                "value2": "39"
+                },
+                {
+                                "hc-key": "Ohio",
+                                "value1": "45",
+                                "value2": "35"
+                },
+                {
+                                "hc-key": "Oklahoma",
+                                "value1": "37",
+                                "value2": "23"
+                },
+                {
+                                "hc-key": "Oregon",
+                                "value1": "37",
+                                "value2": "34"
+                },
+                {
+                                "hc-key": "Pennsylvania",
+                                "value1": "45",
+                                "value2": "36"
+                },
+                {
+                                "hc-key": "Rhode Island",
+                                "value1": "37",
+                                "value2": "32"
+                },
+                {
+                                "hc-key": "South Carolina",
+                                "value1": "36",
+                                "value2": "26"
+                },
+                {
+                                "hc-key": "South Dakota",
+                                "value1": "40",
+                                "value2": "34"
+                },
+                {
+                                "hc-key": "Tennessee",
+                                "value1": "40",
+                                "value2": "29"
+                },
+                {
+                                "hc-key": "Texas",
+                                "value1": "44",
+                                "value2": "32"
+                },
+                {
+                                "hc-key": "Utah",
+                                "value1": "44",
+                                "value2": "38"
+                },
+                {
+                                "hc-key": "Vermont",
+                                "value1": "43",
+                                "value2": "42"
+                },
+                {
+                                "hc-key": "Virginia",
+                                "value1": "47",
+                                "value2": "38"
+                },
+                {
+                                "hc-key": "Washington",
+                                "value1": "47",
+                                "value2": "39"
+                },
+                {
+                                "hc-key": "West Virginia",
+                                "value1": "33",
+                                "value2": "21"
+                },
+                {
+                                "hc-key": "Wisconsin",
+                                "value1": "45",
+                                "value2": "41"
+                },
+                {
+                                "hc-key": "Wyoming",
+                                "value1": "48",
+                                "value2": "35"
+                },
+                {
+                                "hc-key": "District of Columbia",
+                                "value1": "31",
+                                "value2": "19"
+                }
+]
+// JavaScript Document
